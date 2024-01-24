71342d8e766e2f2ef9a12958f012b825ef7dd89a
simonchan66 <formul529@gmail.com>
2024-01-24 13:06:27 -0700

    lab2

---
+++ components/ToDoForm.jsx
@@ -0,0 +1,58 @@
+import React from "react";
+import {
+  SafeAreaView,
+  StyleSheet,
+  Pressable,
+  View,
+  Text,
+  ScrollView,
+  TextInput,
+  Button
+} from 'react-native';
+function ToDoForm(props) {
+  return (
+<View>
+      <View style={[styles.task, styles.completed]}>
+        <Text style={styles.taskText}>Do laundry</Text>
+      </View>
+
+      <View style={[styles.task]}>
+        <Text style={styles.taskText}>Go to gym</Text>
+      </View>
+
+      <View style={[styles.task, styles.completed]}>
+        <Text style={styles.taskText}>Walk dog</Text>
+      </View>
+</View>
+  );
+}
+
+const styles = StyleSheet.create({
+  task: {
+    padding: 10,
+    borderBottomWidth: 1,
+    borderColor: '#ccc',
+  },
+  completed: {
+    backgroundColor: '#e0e0e0',
+  },
+  taskText: {
+    fontSize: 16,
+  },
+  form: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginHorizontal: 20,
+    marginTop: 20,
+  },
+  input: {
+    flex: 1,
+    borderWidth: 1,
+    borderColor: '#ccc',
+    paddingHorizontal: 10,
+    paddingVertical: 5,
+    marginRight: 10,
+  },
+});
+export default ToDoForm;
